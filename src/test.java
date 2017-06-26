7a7c83bc742e3fa2bbd3bbbeb4ddcb79befac21b
ym1549516057 <1549516057@qq.com>
2017-06-26 10:56:48 +0800

    UUID

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+/**
+ * Created by YM on 2017/5/22.
+ */
+public class test {
+    public static void main(String args[]){
+        UUID uuid = UUID.randomUUID();
+        System.out.println("UUID    :"+uuid);
+    }
+
+}
